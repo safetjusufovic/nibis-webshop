f84059e6ce0ad7ebc4826814e17cef306c958a8e
upnew <safetjusuffovic@gmail.com>
2026-05-14 16:09:28 +0200

    fix: build error - konflikt imena u wrapperima

--- src/app/admin/izgled/page.tsx
+++ src/app/admin/izgled/page.tsx
@@ -1,11 +1,11 @@
 'use client'
 import { usePathname } from 'next/navigation'
-import IzgledPage from '@/components/admin/izgled'
+import IzgledPageShared from '@/components/admin/izgled'
 
 export default function IzgledPage() {
   const pathname = usePathname()
   const segs = pathname.split('/').filter(Boolean)
   const idx = segs.indexOf('admin')
   const shopSlug = idx > 0 ? segs[idx - 1] : 'main'
-  return <IzgledPage shopSlug={shopSlug} />
+  return <IzgledPageShared shopSlug={shopSlug} />
 }
--- src/app/admin/korisnici-log/page.tsx
+++ src/app/admin/korisnici-log/page.tsx
@@ -1,11 +1,11 @@
 'use client'
 import { usePathname } from 'next/navigation'
-import KorisniciLogPage from '@/components/admin/korisnici-log'
+import KorisniciLogShared from '@/components/admin/korisnici-log'
 
 export default function KorisniciLogPage() {
   const pathname = usePathname()
   const segs = pathname.split('/').filter(Boolean)
   const idx = segs.indexOf('admin')
   const shopSlug = idx > 0 ? segs[idx - 1] : 'main'
-  return <KorisniciLogPage shopSlug={shopSlug} />
+  return <KorisniciLogShared shopSlug={shopSlug} />
 }
