5a8695a425826508704dafc116324851366997eb
upnew <safetjusuffovic@gmail.com>
2026-05-14 09:31:26 +0200

    debug: console.log u admin/slike

--- src/app/admin/slike/page.tsx
+++ src/app/admin/slike/page.tsx
@@ -12,6 +12,7 @@ export default function AdminSlikePage() {
   const pathSegments = pathname.split('/').filter(Boolean)
   const adminIdx = pathSegments.indexOf('admin')
   const shopSlug = adminIdx > 0 ? pathSegments[adminIdx - 1] : ''
+  console.log('ADMIN SLIKE pathname:', pathname, 'shopSlug:', shopSlug)
 
   const [artikli, setArtikli] = useState<any[]>([])
   const [search, setSearch] = useState('')
